0a5801bc39fed2188df2ea6df02b72e68f6b20f0
arjun523 <67963905+arjun523@users.noreply.github.com>
2021-07-07 17:11:55 +0530

    updated

--- app.py
+++ app.py
@@ -29,15 +29,33 @@ def mybot():
         pin_code = incoming_msg
         date = datetime.datetime.today()
         date = date.strftime("%d-%m-%Y")
+        URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(pin_code,date)
+        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+        string =''
+        counter=0
+        result = requests.get(URL, headers=header)
+        response_json = result.json()
+        data = response_json["sessions"]
+        n=len(data)
+        for num in range(0,len(data)):
+            each = data[num]
+            if(int(each["available_capacity_dose1"])>0):
+                counter +=1
+                string = string + f"Center Name: *{each['name']}*, Address: *{each['address']}*, Minimun Age: *{each['min_age_limit']}*, Vaccine: *{each['vaccine']}*, Fee: *{each['fee_type']}* Total Available Dose Capacity: *{each['available_capacity']}* \t \t  "
+            # with open("abc.txt" ,'a') as file:
+            #     dataf = file.writelines(f"Center Name: *{each['name']}*, Address: *{each['address']}*, Minimun Age: *{each['min_age_limit']}*, Vaccine: *{each['vaccine']}*, Fee: *{each['fee_type']}* Total Available Dose Capacity: *{each['available_capacity']}* \t \t  ")
+            
+        if(counter==0):
+            msg.body("\n*No Available Slots right now!!! Try again tomorrow...*\n")
+        if(counter):
+        # with open('abc.txt','r') as file:
+        #     ndata = file.read()
+        #     return ndata
+            msg.body(string)
         
-        ndata = t.find_avail(pin_code,date)
-        msg.body(ndata)
-        string = ''
-        # with open('abc.txt','w') as file:
-        #     file.write('')
         responded = True
 
-    elif(len(incoming_msg)>6 or incoming_msg.isalnum()):
+    elif(len(incoming_msg)>6 and incoming_msg.isdigit()):
         msg.body("*Type the correct Pin Code*")
 
     else:
